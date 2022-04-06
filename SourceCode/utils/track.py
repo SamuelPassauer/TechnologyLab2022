bdf1e6de59299e16e26d8cbaba5f4819a8fd471b
fionadosdall <56394639+fionadosdall@users.noreply.github.com>
2022-04-06 12:04:50 +0200

    Getter/ Setter

--- SourceCode/utils/track.py
+++ SourceCode/utils/track.py
@@ -8,4 +8,50 @@ class Track:
         self._volume = volume
         self._spectrum = spectrum
 
-    #ToDo Getter setter
+    ##########GetterSetter
+    ######   name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+
+    ##### length 
+    @property
+    def length(self):
+        return self._length
+    
+    @length.setter
+    def length(self, length):
+        self._length=length
+    
+    #### tempo
+    @property
+    def tempo(self):
+        return self._tempo
+    
+    @tempo.setter
+    def tempo(self, tempo):
+        self._tempo=tempo
+
+    #### volume
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @volume.setter
+    def volume(self, volume):
+        self._volume=volume 
+    
+    #### spectrum
+    @property
+    def spectrum(self):
+        return self._spectrum
+    
+    @spectrum.setter
+    def spectrum(self, spectrum):
+       self._spectrum=spectrum
